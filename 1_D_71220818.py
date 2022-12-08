8d8e6b7d2071ba30216e2d31714b7d0860834eba
agasusantooo <117900754+agasusantooo@users.noreply.github.com>
2022-12-08 16:07:03 +0700

    Add files via upload

---
+++ 1_D_71220818.py
@@ -0,0 +1,16 @@
+susu=20000
+permen=500
+roti=15000
+indomie=3000
+vitamin=50000
+
+uang=int(input("Masukkan jumlah uang: Rp"))
+mulai=input("Ketik 'Start' untuk memulai:")
+while(mulai == "start"/"StaRT") :
+    barang=input("Apa barang yang akan Anda beli?")
+    if barang:("permen")
+    sisauang=uang-permen
+    elif barang:("roti")
+    sisauang=uang-roti
+
+print(sisauang)
